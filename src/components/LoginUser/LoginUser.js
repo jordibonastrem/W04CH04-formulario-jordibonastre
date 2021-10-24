0f4759b0f2f9f6e1aa0490b0dda50eeb14ca5097
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-24 22:24:06 +0200

    login user aded

---
+++ src/components/LoginUser/LoginUser.js
@@ -0,0 +1,46 @@
+const LoginUser = () => {
+  return (
+    <form>
+      <h1>Login</h1>
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          className="form-control"
+          placeholder="Enter your username"
+          autoComplete="off"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="form-control"
+          placeholder="Enter your last password"
+          autoComplete="off"
+          required
+        />
+      </div>
+      <div class="form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="remember-password"
+        />
+        <label className="form-check-label" htmlFor="remember-password">
+          Remember password
+        </label>
+      </div>
+      <input type="submit" value="Acceder" className="btn btn-primary" />
+    </form>
+  );
+};
